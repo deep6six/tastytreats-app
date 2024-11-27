30caf4b2d4762f1a99a670d3f9324355692ed083
rIKeCO2 <erikcontreras@csus.edu>
2024-11-26 18:26:49 -0800

    contactPage

--- frontend/src/components/ContactPage.js
+++ frontend/src/components/ContactPage.js
@@ -1,10 +1,23 @@
 
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"; 
+import "./ContactPage.css";
 
 function ContactPage() {
   return (
-    <div>ContactPage
-      <p> logg</p>
+    <div className="whole">
+      <h1 className = "contactHead"> Get In Contact</h1>
+      <div className="contactCont">
+        <div className="contactBox">
+          <div><p>Phone Number: (559)123-4567</p></div>
+        </div>
+        <div className = "emailBox">
+          <div><p>Email: tastytreat@yahoo.com</p></div>
+        </div>
+        <button className = "review">
+          <Link to="/review" className="navbar-link">Leave A Review </Link>
+          </button>
+      </div>
     </div>
     
   )
